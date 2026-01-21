0ad4115e956ecbce4f3181523a8056ed5a624740
minhyeokCho <whalsgur0000@naver.com>
2026-01-21 22:10:28 +0900

    test

--- js/common.js
+++ js/common.js
@@ -1,6 +1,8 @@
 /* 《 스크립트 》 */
 $(document).ready(function(){
+	$('#header').length && headerFixed(); //헤더 고정
 	$('.main_slide').length && mainSlide(); //메인 슬라이드
+	$('.pnr_slide').length && pnrSlide(); //PROJECTS 슬라이드
 });
 
 function mainSlide() {// 메인 슬라이드
@@ -21,3 +23,37 @@ function mainSlide() {// 메인 슬라이드
 	});
 }
 
+function pnrSlide() {// PROJECTS 슬라이드
+	var pnrSlide = new Swiper('.pnr_slide', {
+		slidesPerView : 'auto',
+		loop:true,
+		loopAdditionalSlides : 10,
+		speed:800,
+		spaceBetween: 20, 
+		// autoplay:{
+		//   delay:500,
+		//   disableOnInteraction:false
+		// },
+		slidesPerGroup: 1,
+		grid: {
+			rows: 2,
+			fill: 'row'
+		},
+		navigation : {
+			prevEl : '.arr_prev', // 이번 버튼 클래스명
+			nextEl : '.arr_next', // 다음 버튼 클래스명
+		},
+	});
+}
+
+function headerFixed() {
+	var $header = $('#header');
+
+	$(window).on('scroll.headerFixed', function () {
+		if ($(this).scrollTop() > 0) {
+			$header.addClass('fixed');
+		} else {
+			$header.removeClass('fixed');
+		}
+	});
+}
